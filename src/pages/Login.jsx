a59085d32fa268be7a91658049694dd6545004d5
totatati83@gmail.com <totatati83@gmail.com>
2026-08-04 15:03:48 -0300

    Proteger dashboard con autenticación

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 function Login() {
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
-  return (
-    <div className="container mt-5 text-center">
-      <h1>🏋️ Gym Manager</h1>
-      <p>Bienvenido al sistema</p>
+  const { login } = useAuth();
 
-      <button className="btn btn-primary" onClick={() => navigate("/dashboard")}> 
-        Iniciar Sesión
+  const ingresar = () => {
+    const ok = login(email, password);
+
+    if (ok) {
+      navigate("/dashboard");
+    } else {
+      alert("Usuario o contraseña incorrectos");
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Login</h2>
+
+      <input
+        className="form-control mb-2"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        className="form-control mb-3"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button className="btn btn-primary" onClick={ingresar}>
+        Ingresar
       </button>
     </div>
   );
